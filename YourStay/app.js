a0976892d147cfedf8881a4e0b325c30cd37478a
priyanshugupta1303 <57871920+priyanshugupta1303@users.noreply.github.com>
2022-02-20 13:57:33 +0530

    Add files via upload

---
+++ YourStay/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+const bcrypt = require('bcrypt');
+const path = require("path");
+const bodyParser = require('body-parser');
+const users = require('./data').userDB;
+
+const app = express();
+const server = http.createServer(app);
+
+const app = express();
+const server = http.createServer(app);
